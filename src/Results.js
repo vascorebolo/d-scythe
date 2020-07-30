eb70e61aef1c573ab9d1e9478519ee89dad92ad4
Vasco Silva <vasco.s.rebolo@gmail.com>
2020-07-30 10:00:30 +0100

    Add initial code for faction and boards randomization (wip)

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,47 @@
 import React, { useContext } from 'react'
 import PlayersContext from './contexts/players.context'
 import ExtensionContext from './contexts/extension.context'
-import { BASE_FACTIONS, NEW_FACTIONS, ALL_FACTIONS } from './constants/factions'
+import { BASE_FACTIONS, ALL_FACTIONS } from './constants/factions'
+import { BASE_BOARDS, ALL_BOARDS } from './constants/boards'
+import { div } from 'prelude-ls'
 
 const Results = () => {
   const { players } = useContext(PlayersContext)
   const { extension } = useContext(ExtensionContext)
 
+  const computeResults = () => {
+    const factions = extension ?  ALL_FACTIONS : BASE_FACTIONS
+    const boards = extension ? ALL_BOARDS : BASE_BOARDS
+    let player = 1
+    let results = []
+
+    do {
+      results.push([
+        factions[Math.floor(Math.random() * factions.length)],
+        boards[Math.floor(Math.random() * boards.length)],
+      ]);
+
+      player+=1
+    } while(player <= players)
+
+    return results.map((combo, index) => { return (
+      <div>
+        <h4>Player {index + 1}</h4>
+        <p>
+          <img src={`images/${combo[0].image}`} style={{width: '100px'}} />
+          { combo[0].name } - { combo[1] }
+        </p>
+      </div>
+    )})
+  }
+
   return (
     <div>
       <h1>Results</h1>
       <p>{players} - {extension ? 'yes' : 'no' }</p>
+      <div>
+        { computeResults() }
+      </div>
     </div>
   )
 }
