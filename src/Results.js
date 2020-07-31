8f5ed71ec28cadaad5d3fce43d7f9ae8dcf3d625
Vasco Silva <vasco.s.rebolo@gmail.com>
2020-07-31 19:48:26 +0100

    Improve cards styles

--- src/Results.js
+++ src/Results.js
@@ -6,19 +6,40 @@ import { BASE_BOARDS, ALL_BOARDS } from './constants/boards'
 import bannedCombinations from './constants/banned'
 import styled from 'styled-components'
 
+const ResultsStyled = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
 const PlayerCard = styled.div`
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
   display: flex;
   margin-top: 20px;
+  padding: 20px 40px 20px 20px;
   position: relative;
+  align-items: center;
 
   img {
-    flex-basis: 100px;
-    height: 100px;
+    flex-basis: calc(50px + 10vw);
+    height: calc(50px + 10vw);
   }
 
   .info {
-    margin-left: 20px;
+    margin-left: 5vw;
     text-align: left;
+
+    p {
+      font-size: calc(14px + 1vw);
+      margin: 5px 0;
+
+      b {
+        color: #bb3e1a;
+        text-transform: uppercase;
+      }
+    }
   }
 
   .color {
@@ -27,7 +48,8 @@ const PlayerCard = styled.div`
     height: 30px;
     width: 30px;
     position: absolute;
-    left: 70px;
+    right: 20px;
+    top: 20px;
   }
 `
 
@@ -82,7 +104,7 @@ const Results = ({
         <img alt={combo[0].name} src={`images/${combo[0].image}`} />
         <div className="info">
           <p><b>Player {index + 1}</b></p>
-          <p>{ combo[0].name } - { combo[1] }</p>
+          <p><b>{ combo[0].name } - { combo[1] }</b></p>
           <p>{ combo[0].character }</p>
         </div>
         <span className="color" style={{backgroundColor: combo[0].color}}></span>
@@ -91,13 +113,12 @@ const Results = ({
   }
 
   return (
-    <div>
+    <ResultsStyled>
       <h1>Results</h1>
-      <p>{players} - {extension ? 'yes' : 'no' }</p>
-      <div>
+      <div style={{ width: '95%', maxWidth: '800px' }}>
         { isActive && computeResults() }
       </div>
-    </div>
+    </ResultsStyled>
   )
 }
 
